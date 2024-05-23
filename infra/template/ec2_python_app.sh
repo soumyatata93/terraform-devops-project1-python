c5adc4610cd32a44b10310f4f60b4b5f1e73986d
soumyatata93 <soumyatata@dunsterhouse.co.uk>
2024-05-23 09:21:41 +0100

    dependenices file changed

--- infra/template/ec2_python_app.sh
+++ infra/template/ec2_python_app.sh
@@ -3,9 +3,9 @@
 cd /home/ubuntu
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
-git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
+git clone https://github.com/soumyatata93/python-mysql-terraform-devops-project1.git
 sleep 20
-cd python-mysql-db-proj-1
+cd python-mysql-terraform-devops-project1
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
